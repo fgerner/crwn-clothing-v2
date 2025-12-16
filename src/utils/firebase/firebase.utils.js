3ab82a1204409740656f9386873938147319d6d3
fgerner <fred.gerner@icloud.com>
2025-12-16 21:27:38 +1100

    Added `package-lock.json` for dependency management

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -12,12 +12,12 @@ import {
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyB_lcG7cMomlOV39BL8c58mwWKEKE5_eHQ",
+  authDomain: "fred-clothing-db.firebaseapp.com",
+  projectId: "fred-clothing-db",
+  storageBucket: "fred-clothing-db.firebasestorage.app",
+  messagingSenderId: "294858746359",
+  appId: "1:294858746359:web:30588cf9cdb272b6b3a6f2"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
