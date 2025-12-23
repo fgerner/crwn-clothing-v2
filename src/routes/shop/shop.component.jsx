e221677a892632c730e0a81168d00670032b5e7f
fgerner <fred.gerner@icloud.com>
2025-12-23 15:38:25 +1100

    Add `CategoryPreview` component for category-based product previews; refactor shop page to use new component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -5,23 +5,18 @@ import ProductCard from '../../components/product-card/product-card.component';
 import {CategoriesContext} from '../../contexts/categories.context';
 
 import './shop.styles.scss';
+import CategoryPreview from "../../components/category-preview/category-preview.component";
 
 const Shop = () => {
     const {categoriesMap} = useContext(CategoriesContext);
 
     return (
-        <>
-            {Object.keys(categoriesMap).map(title => (
-                <div key={title}>
-                    <h2>{title}</h2>
-                    <div className='products-container'>
-                        {categoriesMap[title].map((product) => (
-                            <ProductCard key={product.id} product={product}/>
-                        ))}
-                    </div>
-                </div>
-            ))}
-        </>
+        <div className='shop-container'>
+            {Object.keys(categoriesMap).map(title => {
+                const products = categoriesMap[title];
+                return <CategoryPreview key={title} title={title} products={products}/>
+            })}
+        </div>
     );
 };
 
