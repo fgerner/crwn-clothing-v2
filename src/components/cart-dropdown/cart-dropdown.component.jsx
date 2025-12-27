180921037be94dcf85f30dd0165fba94d95ee0d3
fgerner <fred.gerner@icloud.com>
2025-12-27 18:17:01 +1100

    Refactor cart dropdown to use `styled-components` and update styles for enhanced maintainability.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,8 +5,9 @@ import {CartContext} from '../../contexts/cart.context';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 
-import './cart-dropdown.styles.scss';
+import './cart-dropdown.styles';
 import {useNavigate} from "react-router-dom";
+import {EmptyMessage} from "./cart-dropdown.styles";
 
 const CartDropdown = () => {
     const {cartItems} = useContext(CartContext);
@@ -17,18 +18,18 @@ const CartDropdown = () => {
     }
 
     return (
-        <div className='cart-dropdown-container'>
-            <div className='cart-items'>
+        <CartDropdown>
+            <CartItem>
                 {cartItems.length ? (
                     cartItems.map((cartItem) => (
                         <CartItem key={cartItem.id} cartItem={cartItem}/>
                     ))
                 ) : (
-                    <span className='empty-message'>Your cart is empty</span>
+                    <EmptyMessage>Your cart is empty</EmptyMessage>
                 )}
-            </div>
+            </CartItem>
             <Button onClick={checkoutHandler}>GO TO CHECKOUT</Button>
-        </div>
+        </CartDropdown>
     );
 };
 
