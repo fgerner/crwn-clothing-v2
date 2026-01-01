10eaef95f58395efde133d063c5536a89a549742
fgerner <fred.gerner@icloud.com>
2026-01-01 20:45:24 +1100

    Introduce reducers for `CartContext` and `UserContext` and update favicon to crown.svg

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,29 +1,46 @@
-import { createContext, useState, useEffect } from 'react';
+import {createContext, useEffect, useReducer} from 'react';
 
 import {
-  onAuthStateChangedListener,
-  createUserDocumentFromAuth,
+    onAuthStateChangedListener,
+    createUserDocumentFromAuth,
 } from '../utils/firebase/firebase.utils';
 
 export const UserContext = createContext({
-  setCurrentUser: () => null,
-  currentUser: null,
+    setCurrentUser: () => null,
+    currentUser: null,
 });
 
-export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  const value = { currentUser, setCurrentUser };
+export const USER_ACION_TYPES = {SET_CURRENT_USER: 'SET_CURRENT_USER'};
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-      setCurrentUser(user);
-    });
+const userReducer = (state, action) => {
+    const {type, payload} = action;
+    switch (type) {
+        case USER_ACION_TYPES.SET_CURRENT_USER:
+            return {...state, currentUser: payload};
+        default:
+            throw new Error(`Unhandled action type: ${type}`);
+    }
+};
 
-    return unsubscribe;
-  }, []);
+const initialState = {currentUser: null};
 
-  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+export const UserProvider = ({children}) => {
+    const [{currentUser}, dispatch] = useReducer(userReducer, initialState)
+
+    const setCurrentUser = (user) => dispatch({type: USER_ACION_TYPES.SET_CURRENT_USER, payload: user})
+
+    const value = {currentUser, setCurrentUser};
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedListener((user) => {
+            if (user) {
+                createUserDocumentFromAuth(user);
+            }
+            setCurrentUser(user);
+        });
+
+        return unsubscribe;
+    }, []);
+
+    return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
